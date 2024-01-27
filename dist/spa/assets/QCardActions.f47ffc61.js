f23b23f88cf4d4a10fbe99905c93179684938580
Michail Tsimpliarakis <m.tsimpliarakis@gmail.com>
2024-01-27 21:25:35 +0200

    gh-pages stuff

---
+++ dist/spa/assets/QCardActions.f47ffc61.js
@@ -0,0 +1,1 @@
+import{c as t,aI as r,aJ as c,a as n,h as l,d as i}from"./index.fbbb49e6.js";var d=t({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=n(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:o.value},i(s.default))}});export{d as Q};
---
+++ dist/spa/assets/axios.ba53325e.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.fbbb49e6.js";import{a}from"./axios.0d4d808a.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
