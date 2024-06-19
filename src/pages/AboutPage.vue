51d3c3082f9d9dac523f9620366f698a53110f6c
Michail Tsimpliarakis <m.tsimpliarakis@gmail.com>
2024-06-19 03:18:55 +0300

    added about page

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,63 @@
+<!-- About Page -->
+<template>
+  <q-page class="flex flex-center">
+    <div class="page">
+      <div
+        class="text-h3 text-center"
+        style="margin-bottom: 50px; margin-top: 50px"
+      >
+        About Us
+      </div>
+      <div>
+        <p class="text-weight-thin">About MyCarSharing</p>
+        <p>
+          MyCarSharing aims to revolutionize how people commute by offering a
+          platform for users to share their vehicles with others. Whether you're
+          looking to earn extra income by renting out your car or seeking a
+          convenient way to access vehicles, MyCarSharing connects you with a
+          community of car owners and users.
+        </p>
+
+        <h4>Our Mission</h4>
+        <p>
+          Our mission is to provide a sustainable and cost-effective alternative
+          to traditional transportation methods. We believe in reducing vehicle
+          ownership costs, promoting environmental sustainability, and enhancing
+          community connectivity through shared mobility solutions.
+        </p>
+
+        <h4>Company Overview</h4>
+        <p>
+          MyCarSharing was founded in 2023 and is headquartered in Kerkyra. As a
+          leading car-sharing platform, we facilitate thousands of transactions
+          daily, ensuring safe and reliable vehicle sharing experiences for our
+          users across [regions/countries].
+        </p>
+
+        <h4>Meet Our Team</h4>
+        <p>
+          At MyCarSharing, we are a dedicated team of professionals committed to
+          transforming urban mobility. From our visionary leaders to our
+          passionate developers and customer support specialists, each member of
+          our team contributes to making car sharing accessible and seamless.
+        </p>
+
+        <h4>Contact Us</h4>
+        <p>
+          Have questions or feedback? We'd love to hear from you! Contact our
+          support team at info@mycarsharing.com or give us a call at +30 1234
+          5678. Our office is located at Kerkyra, Ionian Islands, Greece.
+        </p>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<style>
+.page {
+  margin-left: 10%;
+  margin-right: 10%;
+  max-width: 800px;
+  margin-bottom: 50px;
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -129,7 +129,16 @@ const routes = [
       },
     ],
   },
-
+  {
+    path: "/about",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("src/pages/AboutPage.vue"),
+      },
+    ],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
