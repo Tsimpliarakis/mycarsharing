227fdb1ebf20ae67584bf9b01ea179febc345285
Michail Tsimpliarakis <m.tsimpliarakis@gmail.com>
2024-08-07 22:21:06 +0300

    file deletions

--- src/components/account/LeftPanelMenu.vue
+++ src/components/account/LeftPanelMenu.vue
@@ -22,51 +22,53 @@
       <div class="q-mt-md">
         <q-separator />
         <q-item>
-          <q-item-section
-            ><q-btn flat rounded @click="togglePanel('ChangeAvatar')"
-              >Change Avatar</q-btn
-            ></q-item-section
-          >
+          <q-item-section>
+            <q-btn flat rounded @click="togglePanel('ChangeAvatar')">
+              Change Avatar
+            </q-btn>
+          </q-item-section>
         </q-item>
         <q-item>
-          <q-item-section
-            ><q-btn flat rounded @click="togglePanel('ProfileInfo')"
-              >Profile Information</q-btn
-            ></q-item-section
-          >
+          <q-item-section>
+            <q-btn flat rounded @click="togglePanel('ProfileInfo')">
+              Profile Information
+            </q-btn>
+          </q-item-section>
         </q-item>
         <q-item v-if="!authStore.state.profile.is_verified">
-          <q-item-section
-            ><q-btn flat rounded @click="togglePanel('UploadDocuments')"
-              >Upload documents</q-btn
-            ></q-item-section
-          >
+          <q-item-section>
+            <q-btn flat rounded @click="togglePanel('UploadDocuments')">
+              Upload Documents
+            </q-btn>
+          </q-item-section>
         </q-item>
         <q-item>
-          <q-item-section
-            ><q-btn flat rounded @click="togglePanel('ChangePassword')"
-              >Change Password</q-btn
-            ></q-item-section
-          >
+          <q-item-section>
+            <q-btn flat rounded @click="togglePanel('ChangePassword')">
+              Change Password
+            </q-btn>
+          </q-item-section>
         </q-item>
         <q-separator />
         <q-item>
-          <q-item-section
-            ><q-btn
+          <q-item-section>
+            <q-btn
               flat
               rounded
               color="red-7"
               label="Delete Account"
               @click="confirmDialog = true"
-          /></q-item-section>
+            />
+          </q-item-section>
           <q-dialog v-model="confirmDialog" persistent>
             <q-card>
               <q-card-section class="row items-center">
                 <q-avatar icon="warning" color="red-7" text-color="white" />
                 <div class="column q-ml-sm">
-                  <span
-                    >Are you sure you want to permanently delete your account? </span
-                  ><span> This action cannot be undone.</span>
+                  <span>
+                    Are you sure you want to permanently delete your account?
+                  </span>
+                  <span> This action cannot be undone.</span>
                 </div>
               </q-card-section>
 
@@ -104,7 +106,6 @@ import { useRouter } from "vue-router";
 import { useQuasar } from "quasar";
 
 const $q = useQuasar();
-
 const router = useRouter();
 
 const { togglePanel: parentTogglePanel } = defineProps(["togglePanel"]);
@@ -113,16 +114,62 @@ const togglePanel = (panelName) => {
   parentTogglePanel(panelName);
 };
 
-// Define deleteUserAccount as a ref
+async function deleteUserFiles(userId) {
+  const buckets = ["cars"];
+  try {
+    for (const bucket of buckets) {
+      const { data: files, error: listError } = await supabase.storage
+        .from(bucket)
+        .list();
+      if (listError) throw listError;
+      const filesToDelete = files.filter((file) =>
+        file.name.startsWith(userId)
+      );
+      const filePaths = filesToDelete.map((file) => file.name);
+      const { error: removeError } = await supabase.storage
+        .from(bucket)
+        .remove(filePaths);
+      if (removeError) throw removeError;
+    }
+  } catch (error) {
+    $q.notify({
+      color: "red-5",
+      textColor: "white",
+      icon: "report_problem",
+      message: `Error deleting user files: ${error.message}`,
+      position: "top",
+    });
+  }
+}
+
 async function deleteUserAccount() {
-  await supabase.rpc("delete_user");
-  await logoutSession();
+  const userId = authStore.state.session.user.id;
+  try {
+    const { error } = await supabase.rpc("delete_user");
+    if (error) throw error;
+    await deleteUserFiles(userId);
+    await logoutSession();
+    $q.notify({
+      color: "green-8",
+      textColor: "white",
+      icon: "check_circle",
+      message: "User account and files deleted successfully.",
+      position: "top",
+    });
+  } catch (error) {
+    $q.notify({
+      color: "red-5",
+      textColor: "white",
+      icon: "report_problem",
+      message: `Error deleting user account: ${error.message}`,
+      position: "top",
+    });
+  }
 }
 
 const logoutSession = async () => {
   const { error } = await supabase.auth.signOut();
   authStore.mutations.resetStates(authStore.state);
-
   if (error) {
     $q.notify({
       color: "red-5",
@@ -139,11 +186,9 @@ const logoutSession = async () => {
       message: "Logged out successfully",
       position: "top",
     });
-
     router.push("/");
   }
 };
 
-// Define confirmDialog as a ref
 const confirmDialog = ref(false);
 </script>
