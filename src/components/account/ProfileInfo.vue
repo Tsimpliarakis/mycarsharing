3355c8dd25aaaa3d66564258cee9993e3bc53531
Michail Tsimpliarakis <m.tsimpliarakis@gmail.com>
2024-08-07 22:55:24 +0300

    added birth date

--- src/components/account/ProfileInfo.vue
+++ src/components/account/ProfileInfo.vue
@@ -50,6 +50,21 @@
       />
       <br />
       <div class="input_field">
+        <p style="text-align: left; font-weight: bold">Date of Birth:</p>
+        <q-input
+          v-model="dateOfBirth"
+          color="green-7"
+          type="date"
+          placeholder="YYYY-MM-DD"
+        />
+      </div>
+      <br />
+      <q-separator
+        color="green-3"
+        style="width: 90%; height: 2px; margin-left: 5%"
+      />
+      <br />
+      <div class="input_field">
         <p style="text-align: left; font-weight: bold">Description:</p>
         <q-input
           v-model="description"
@@ -81,21 +96,25 @@ const description = ref("");
 const phoneNumber = ref("");
 const firstName = ref("");
 const lastName = ref("");
+const dateOfBirth = ref(""); // Added date of birth field
 const submitting = ref(false);
 const $q = useQuasar();
 
 async function submitInfo() {
   submitting.value = true;
 
+  // Construct an object with only the fields that have values
+  const updatedFields = {};
+  if (firstName.value) updatedFields.first_name = firstName.value;
+  if (lastName.value) updatedFields.last_name = lastName.value;
+  if (phoneNumber.value) updatedFields.phone = phoneNumber.value;
+  if (dateOfBirth.value) updatedFields.date_of_birth = dateOfBirth.value;
+  if (description.value) updatedFields.description = description.value;
+
   try {
     const { data, error } = await supabase
       .from("profiles")
-      .update({
-        description: description.value,
-        phone: phoneNumber.value,
-        first_name: firstName.value,
-        last_name: lastName.value,
-      })
+      .update(updatedFields)
       .eq("id", authStore.state.session.user.id);
 
     if (error) throw error;
